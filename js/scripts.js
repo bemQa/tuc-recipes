d789ab45913d2b895f4ff757d1724ba5218fe8a8
Alexander K <niktonezapomnil@gmail.com>
2020-12-08 12:51:44 +0500

    animations

--- js/scripts.js
+++ js/scripts.js
@@ -83,6 +83,8 @@ $(document).ready(function () {
             ]
         });
     }
+
+    scrollWaypointInit($('.animateMe'));
 });
 
 ( function() {
@@ -113,3 +115,26 @@ $(document).ready(function () {
     };
     
 } )();
+
+function scrollWaypointInit(items, trigger) {
+    items.each(function() {
+        var element = $(this),
+            osAnimationClass = element.data("animation"),
+            osAnimationDelay = element.attr('data-animation-delay');
+
+        element.css({
+            '-webkit-animation-delay': osAnimationDelay,
+            '-moz-animation-delay': osAnimationDelay,
+            'animation-delay': osAnimationDelay
+        });
+
+        var trigger = (trigger) ? trigger : element;
+
+        trigger.waypoint(function() {
+            element.addClass('animated').addClass(osAnimationClass);
+        }, {
+            // triggerOnce: true,
+            offset: '80%'
+        });
+    });
+}
